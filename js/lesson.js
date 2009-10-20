b014a6bec185e83b33ccb39b890a5fd73b3ab1a5
Bryan Berry <bryan@olenepal.org>
2009-10-20 12:20:21 -0700

    scaling now works automatically

--- js/lesson.js
+++ js/lesson.js
@@ -1,54 +1,49 @@
 $(document).ready(function(){
-    var DEFAULT_SIZE_X = 786;
-    var DEFAULT_SIZE_Y = 900;
-    //stupid hack, find out how to calc the diff between image and top
-    // of webpage
-    var OFFSET = 8;
-    var g = game();
+    
+    var SVG_EMBED = document.getElementById('mysvg');
+    var MAX_SCREEN_X = 1200;
+    var MAX_SCREEN_Y = 900;
 
-    var k = $.karma({container: "#karma-main"})
-    k.init({
-	images : [
-	    {name : "background", file : "fondo.png", localized : false },
-	    {name : "capital", file : "capital.png" , localized : false},
-	    {name : "lines", file : "deptosLineas.png", localized: false}
-	]
-    });
+    // You can't access any properties of the svg document 
+    // until it has loaded
+    $('#mysvg').bind('load', function() {
+	var SVG_DOC = SVG_EMBED.getSVGDocument();
+	/*  var myCity = svgDoc.getElementById('capArtigas')
+	      myCity.addEventListener('click', 
+	      function(){ document.write('foo');}, false);
+        */
 
-    k.main(function() {
-     	var paper = Raphael("mycanvas",DEFAULT_SIZE_X,DEFAULT_SIZE_Y);
-
-	var g = game(paper,k.library.images["capital"].src);
-	var q = g.newquestion();
-	$('#question').html(q.getPhrase);
-
+	$('.text', SVG_DOC).attr('display', 'none');
+  	$.map($('.capital.city', SVG_DOC), function(elem){
+	    $(elem, SVG_DOC).bind('click', function() { document.write('foo');
+						     })});
 	
-	var clicked = function(event){
-	    var x = event.clientX + document.documentElement.scrollLeft - OFFSET;
-	    var y = event.clientY + document.documentElement.scrollTop - OFFSET;
-	    var ans = g.isAnswerp(x, y);
-	    if (ans) { 
-		g.draw();
-		q = g.newquestion();
+	
+
+	var scaleView = function (svgElem, width, height) {
+	    var newRatio = 1;
+	    var xRatio = width/MAX_SCREEN_X;
+	    var yRatio = height/MAX_SCREEN_Y;
+
+	    //get the smallest ratio
+	    newRatio = xRatio > yRatio ? yRatio : xRatio;
+
+	    if (newRatio > 1) {
+		//do nothing
+		return newRatio;
+	    } else {
+		svgElem.currentScale = newRatio - 0.05;
+		return newRatio;
 	    }
-	    $('question').html(q.getPhrase);
-	}
+	};
 
-	var draw = function() {
-	    paper.image(k.library.images["background"].src,0,0,DEFAULT_SIZE_X,DEFAULT_SIZE_Y);
-	    var i = paper.image(k.library.images["lines"].src,0,0,DEFAULT_SIZE_X,DEFAULT_SIZE_Y);
-	    g.draw();
-	}
+	var hideElements = function () {
+	
+	};
+      
+	scaleView(SVG_DOC.documentElement, window.innerWidth, 
+		  window.innerHeight);
 
-	$('#mycanvas').bind('click', clicked, false);
-
-	$('#start').bind('click', draw, false);
-     
     });
 });
-	
 
-
-	    
-
-    
